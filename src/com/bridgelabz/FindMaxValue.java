e549206dd99d6ce4ca1d461b22209e3b33777e71
Sakshi <sakshishetty1231997@gmail.com>
2021-08-17 15:34:29 +0530

    Added: To find maximum of three float

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -6,7 +6,7 @@ package com.bridgelabz;
  * @since - 2021-08-17
  */
 public class FindMaxValue {
-	
+
 	/**
 	 * Method used to compare three integer values and get the maximum value.
 	 * @param a1 : First value to compare
@@ -23,9 +23,28 @@ public class FindMaxValue {
 		return max;
 	}
 
+	/**
+	 * Method to compare three float values and get the maximum value.
+	 * @param f1 : First value to compare.
+	 * @param f2 : Second value to compare.
+	 * @param f3 : Third value to compare.
+	 * @return max : Maximum of three float
+	 */
+
+	public static Float maxOfFloat(Float f1, Float f2, Float f3) {
+		Float max = f1;
+		if (f2.compareTo(max) > 0)
+			max = f2;
+		if (f3.compareTo(max) > 0)
+			max = f3;
+		return max;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the program to find maximum value using generics");
 		Integer a1 = 20, a2 = 8, a3 = 12;
 		System.out.println("The Maximum between three integers is : " + maxOfInteger(a1, a2, a3));
+		Float f1 = 1.5f, f2 = 8.2f, f3 = 6.4f;
+		System.out.println("The Maximum between three float is : " + maxOfFloat(f1, f2, f3));
 	}
 }
