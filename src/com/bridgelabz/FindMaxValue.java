259eaacfcac583032b3f02c336b809442545bc5c
Sakshi <sakshishetty1231997@gmail.com>
2021-08-17 15:54:00 +0530

    Refactor: Used a generic method to find maximum

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -8,14 +8,14 @@ package com.bridgelabz;
 public class FindMaxValue {
 
 	/**
-	 * Method used to compare three integer values and get the maximum value.
+	 * Generic method to compare any type of data and find maximum
 	 * @param a1 : First value to compare
 	 * @param a2 : Second value to compare
 	 * @param a3 : Third value to compare
-	 * @return max : Maximum of three integers
+	 * @return max : Maximum of three values
 	 */
-	public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3) {
-		Integer max = a1;
+	public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+		T max = a1;
 		if (a2.compareTo(max) > 0)
 			max = a2;
 		if (a3.compareTo(max) > 0)
@@ -23,47 +23,13 @@ public class FindMaxValue {
 		return max;
 	}
 
-	/**
-	 * Method to compare three float values and get the maximum value.
-	 * @param f1 : First value to compare.
-	 * @param f2 : Second value to compare.
-	 * @param f3 : Third value to compare.
-	 * @return max : Maximum of three float
-	 */
-
-	public static Float maxOfFloat(Float f1, Float f2, Float f3) {
-		Float max = f1;
-		if (f2.compareTo(max) > 0)
-			max = f2;
-		if (f3.compareTo(max) > 0)
-			max = f3;
-		return max;
-	}
-
-	/**
-	 * Method to compare three strings and get the maximum value. 
-	 * @param s1 : First value to compare.
-	 * @param s2 : Second value to compare.
-	 * @param s3 : Third value to compare.
-	 * @return max : Maximum of three strings
-	 */
-
-	public static String maxOfString(String s1, String s2, String s3) {
-		String max = s1;
-		if (s2.compareTo(max) > 0)
-			max = s2;
-		if (s3.compareTo(max) > 0)
-			max = s3;
-		return max;
-	}
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to the program to find maximum value using generics");
 		Integer a1 = 20, a2 = 8, a3 = 12;
-		System.out.println("The Maximum between three integers is : " + maxOfInteger(a1, a2, a3));
+		System.out.println("The Maximum between three integers is : " + maxOfValues(a1, a2, a3));
 		Float f1 = 1.5f, f2 = 8.2f, f3 = 6.4f;
-		System.out.println("The Maximum between three float is : " + maxOfFloat(f1, f2, f3));
+		System.out.println("The Maximum between three float is : " + maxOfValues(f1, f2, f3));
 		String s1 = "abc", s2 = "pqr", s3 = "xyz";
-		System.out.println("The Maximum between three float is : " + maxOfString(s1, s2, s3));
+		System.out.println("The Maximum between three float is : " + maxOfValues(s1, s2, s3));
 	}
 }
