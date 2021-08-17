13a4517c976234d389e3aed1aa802bd980f30684
Sakshi <sakshishetty1231997@gmail.com>
2021-08-17 15:15:38 +0530

    Added: Welcome message to the program

---
+++ src/com/bridgelabz/FindMaxValue.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To find maximum value using generics
+ * @author - Sakshi Shetty
+ * @version - 8.0
+ * @since - 2021-08-17
+ */
+public class FindMaxValue {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the program to find maximum value using generics");
+	}
+}
