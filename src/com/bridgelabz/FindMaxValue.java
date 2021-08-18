761e996e891ebcd041da80747dc0bbf0f7e43420
Sakshi <sakshishetty1231997@gmail.com>
2021-08-18 12:14:09 +0530

    Refactor: Using Standard output format in comparing method

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -24,16 +24,17 @@ public class FindMaxValue<T extends Comparable<T>> {
 		Arrays.sort(elements);
 		int lenght = elements.length;
 		T max = elements[lenght - 1];
+		System.out.printf("Maxium value of three is %s :  ", max);
 		return max;
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the program to find maximum value using generics");
 		Integer[] intMax = { 10, 24, 68, 80, 42, 46, 12, 58 };
-		System.out.println("The Maximum between three integers is : " + maxOfValues(intMax));
+		maxOfValues(intMax);
 		Float[] floatMax = { 1.4f, 87.3f, 81.8f, 40.2f, 4.6f };
-		System.out.println("The Maximum between three float is : " + maxOfValues(floatMax));
+		maxOfValues(floatMax);
 		String[] stringMax = { "ABC", "IJK", "PQR", "BCD", "MNO", "LMN", "RST", "XYZ" };
-		System.out.println("The Maximum between three String is : " + maxOfValues(stringMax));
+		maxOfValues(stringMax);
 	}
 }
